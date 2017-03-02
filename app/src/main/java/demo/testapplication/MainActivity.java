8b840a77e69a8a43236b0513c24b11115321a23d
xu.wang <867103701@qq.com>
2017-03-02 23:02:48 +0800

    Initial commit

---
+++ app/src/main/java/demo/testapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package demo.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by：xu.wang on 2017/3/2 22:53
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
